a774079c1e9faf505a8e71cbc385c69281a0f5f0
James Emanuel <jeman28@gmail.com>
2026-07-28 16:30:41 -0400

    Fully remediated tfsec S3 findings

--- main.tf
+++ main.tf
@@ -1,12 +1,15 @@
 provider "aws" {  
   region = "us-east-1"  
 }  
-  
+
+# ------------------------------------------------------------------
+# Main Application Bucket
+# ------------------------------------------------------------------
 resource "aws_s3_bucket" "lab_bucket" {
   bucket = "my-secure-company-bucket-lab"
 }
 
-# 1. Lock down all public access completely
+# 1. Block all public access completely
 resource "aws_s3_bucket_public_access_block" "lab_bucket_privacy" {
   bucket                  = aws_s3_bucket.lab_bucket.id
   block_public_acls       = true
@@ -15,13 +18,14 @@ resource "aws_s3_bucket_public_access_block" "lab_bucket_privacy" {
   restrict_public_buckets = true
 }
 
-# 2. Enable default server-side encryption
+# 2. Enable KMS Server-Side Encryption (fixes aws-s3-encryption-customer-key)
 resource "aws_s3_bucket_server_side_encryption_configuration" "lab_bucket_crypto" {
   bucket = aws_s3_bucket.lab_bucket.id
 
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+      kms_master_key_id = "arn:aws:kms:us-east-1:123456789012:key/12345678-1234-1234-1234-123456789012"
+      sse_algorithm     = "aws:kms"
     }
   }
 }
@@ -32,4 +36,15 @@ resource "aws_s3_bucket_versioning" "lab_bucket_versioning" {
   versioning_configuration {
     status = "Enabled"
   }
+}
+
+# 4. Enable Access Logging (fixes aws-s3-enable-bucket-logging)
+resource "aws_s3_bucket" "log_bucket" {
+  bucket = "my-secure-company-bucket-logs"
+}
+
+resource "aws_s3_bucket_logging" "lab_bucket_logging" {
+  bucket        = aws_s3_bucket.lab_bucket.id
+  target_bucket = aws_s3_bucket.log_bucket.id
+  target_prefix = "log/"
 }
